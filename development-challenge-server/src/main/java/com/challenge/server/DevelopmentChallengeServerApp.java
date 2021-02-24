85715bf57685d44f95a24003ca7f3eceb830692f
Tung Bui Thanh <tungbuithanh1977@gmail.com>
2021-02-24 09:25:58 +0700

    Implementation full assignment V2

--- development-challenge-server/src/main/java/com/challenge/server/DevelopmentChallengeServerApp.java
+++ development-challenge-server/src/main/java/com/challenge/server/DevelopmentChallengeServerApp.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DevelopmentChallengeServerApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevelopmentChallengeServerApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentChallengeServerApp.class, args);
+    }
 }
